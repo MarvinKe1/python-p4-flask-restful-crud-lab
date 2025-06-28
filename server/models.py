ce4b96b3c30f12ee878d5e507ffa618afa966c4b
Marvin Mango <marvinke2001@gmail.com>
2025-06-28 12:04:10 +0300

    REST CRUD Lab

--- server/models.py
+++ server/models.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
+class Plant(db.Model):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,4 +12,13 @@ class Plant(db.Model, SerializerMixin):
     is_in_stock = db.Column(db.Boolean)
 
     def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+        return f'<Plant {self.name}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+            'is_in_stock': self.is_in_stock
+        }
